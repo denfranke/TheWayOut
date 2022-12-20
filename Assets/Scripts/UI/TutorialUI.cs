ed0d91d8fc7449540e019b9f9111b43de51be220
XoDefender <kir.zhilnikov37@internet.ru>
2022-12-20 22:56:01 +0300

    Added tutorial UI

---
+++ Assets/Scripts/UI/TutorialUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialUI : MonoBehaviour
+{
+    public void OnPlayButtonClick()
+    {
+        gameObject.SetActive(false);
+    }
+}
