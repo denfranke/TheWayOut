24b8f8d419c29393e73d504f5d2fba4192c22b0a
XoDefender <kir.zhilnikov37@internet.ru>
2023-01-05 20:19:17 +0300

    Dropped cards to map grid

--- Assets/Scripts/UI/UnitCards/UnitCardSlot.cs
+++ Assets/Scripts/UI/UnitCards/UnitCardSlot.cs
@@ -2,10 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System;
 
 public class UnitCardSlot : MonoBehaviour, IDropHandler
 {
-    public void OnDrop(PointerEventData eventData)
+    public static event EventHandler<OnAnyDropToSlotEventArgs> OnAnyDropToSlot;
+
+    public class OnAnyDropToSlotEventArgs : EventArgs
     {
+        public RectTransform rectTransformCard;
+        public RectTransform rectTransformSlot;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    { 
+        if(eventData.pointerDrag != null)
+        {
+            OnAnyDropToSlot?.Invoke(this, new OnAnyDropToSlotEventArgs
+            {
+                rectTransformCard = eventData.pointerDrag.GetComponent<RectTransform>(),
+                rectTransformSlot = GetComponent<RectTransform>()
+            });
+        }
     }
 }
